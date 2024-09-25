aa7bfd8c46d7ae511489cb487347e5011e1ce241
Javier Caicedo Tobon <javiercaicedotobon@gmail.com>
2024-09-25 13:16:40 -0500

    Actualizacion de la vista pago virtual, se anexa a la programacion la validacion de saldo actual para poder llevar acabo la transaccion

--- bankapp/applications/transaction/views.py
+++ bankapp/applications/transaction/views.py
@@ -294,35 +294,46 @@ class VirtualPayView(APIView):
         card = Card.objects.get(user=user)
         account = Account.objects.get(user_id=user)
         operation = Operation.objects.get(id=serializer.validated_data['operation'])
+
+        if card.balance >= serializer.validated_data['transaction_amount']:    
+            transaction = Transaction.objects.create(
+                card = card,
+                account = account,
+                user = user,
+                operation = operation,
+                transaction_date = datetime.datetime.now(),
+                destination_account = serializer.validated_data['destination_account'],
+                transaction_amount = serializer.validated_data['transaction_amount']
+            )
+
+            account_desstiantion = Account.objects.get(account_number=transaction.destination_account)
+            card_destination = Card.objects.get(account=account_desstiantion)
+            card_destination.balance += transaction.transaction_amount
+            card_destination.save()
+
+
+            transaction.card.balance -= transaction.transaction_amount
+            transaction.card.save()
+
+
+            return Response(
+                {
+                    'status' : status.HTTP_200_OK,
+                    'message' : 'Pago virtual realizado',
+                    'account' : transaction.destination_account,
+                    'amount' : transaction.transaction_amount
+                }
+            )
         
-        transaction = Transaction.objects.create(
-            card = card,
-            account = account,
-            user = user,
-            operation = operation,
-            transaction_date = datetime.datetime.now(),
-            destination_account = serializer.validated_data['destination_account'],
-            transaction_amount = serializer.validated_data['transaction_amount']
-        )
+        else:
+            return Response(
+                {
+                    'status' : status.HTTP_401_UNAUTHORIZED,
+                    'message' : 'No se pudo realizar la operacion, no tiene fondos suficientes',
+                    'saldo_actual' : card.balance
+                }
+            )
 
-        account_desstiantion = Account.objects.get(account_number=transaction.destination_account)
-        card_destination = Card.objects.get(account=account_desstiantion)
-        card_destination.balance += transaction.transaction_amount
-        card_destination.save()
-
-
-        transaction.card.balance -= transaction.transaction_amount
-        transaction.card.save()
-
-
-        return Response(
-            {
-                'status' : status.HTTP_200_OK,
-                'message' : 'Pago virtual realizado',
-                'account' : transaction.destination_account,
-                'amount' : transaction.transaction_amount
-            }
-        )
 
 
 
